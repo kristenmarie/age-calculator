dde069f657ff7ef5309d54d64b28e5ac59b16e06
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-10-13 13:09:38 -0700

    Add form to index.html.

---
+++ js/ageCalculator-interface.js
@@ -0,0 +1,7 @@
+import {AgeCalculator} from './../js/ageCalculator.js';
+
+$(document).ready(function(){
+  $('form#user-information').submit(function(event){
+    event.preventDefault();
+  });
+});
