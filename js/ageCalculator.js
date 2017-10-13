bc688673123935ec474353547a1e76e5d4ae0de2
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-10-13 10:41:53 -0700

    Finally add method to determine time different between dates in seconds.

--- js/ageCalculator.js
+++ js/ageCalculator.js
@@ -1,10 +1,24 @@
+
 export class AgeCalculator{
 
-  constructor(age){
-    this.age = age;
+  constructor(birthday){
+    this.birthday = birthday;
   }
 
-  convertAge(){
-    return (this.age * 31556952)
+  // convertAge(){
+  //   let age = this.timeDifference(this.birthday);
+  //   return (age * 31556952);
+  // }
+
+  timeDifference(){
+    let birthday = this.birthday.replace(/\//g, '');
+    let year = parseInt(birthday.slice(0,4));
+    let month = parseInt(birthday.slice(4,6));
+    let day = parseInt(birthday.slice(6,8));
+    let date = new Date();
+    let yearDif = date.getFullYear() - year;
+    let monthDif = (date.getMonth() + 1) - month;
+    let difference = (yearDif * 31556952) + (monthDif * 2592000);
+    return difference;
   }
 }
--- spec/ageCalculator-spec.js
+++ spec/ageCalculator-spec.js
@@ -3,14 +3,16 @@ import {AgeCalculator} from './../js/ageCalculator.js';
 describe('AgeCalculator', function(){
   let testCalculator;
   beforeEach(function(){
-    testCalculator = new AgeCalculator(28);
+    testCalculator = new AgeCalculator('1989/05/29');
   });
 
-  it('will convert an age from years to seconds', function(){
-    expect(testCalculator.convertAge()).toEqual(883594656);
-  });
 
   it('will take two dates and determine the difference between the two in seconds', function(){
-
+    expect(testCalculator.timeDifference()).toEqual(896554656);
   });
+
+  // it('will convert an age from years to seconds', function(){
+  //   expect(testCalculator.convertAge()).toEqual(883594656);
+  // });
+
 });
